1fc1cc463a4f56cb3f24b1ef17efd0f4ec9e4164
Bao Bo <bob432091@gmail.com>
2023-12-13 12:53:44 +0800

    remove hidden properties from $Scope

--- src/core.ts
+++ src/core.ts
@@ -1,18 +1,15 @@
 export function $<P extends Array<any>, R>(func: (...p: P) => R, onEffect?: $EffectHandlerCreator, scope?: $Scope): (...p: P) => R {
-    const stack = scope?.[$StackKey] ?? new Array<$Variable<any>>();
-    const handler = onEffect?.(currentScope?.handler ?? null) ?? null;
+    scope = scope ?? new Array<$Variable<any>>()
+    const handler = onEffect?.(currentHandler ?? null) ?? null;
 
     return (...p: P): R => {
-        currentScope = {
-            [$StackKey]: stack,
-            parent: null,
-            cursor: 0,
-            handler: handler,
-        }
+        const context = getContext();
+        setContext([scope!, 0, handler])
         const result = func(...p);
-        if (currentScope?.parent !== null) {
+        if (currentScope !== scope) {
             throw ('Scope not close.');
         }
+        setContext(context);
         return result;
     };
 }
@@ -20,25 +17,20 @@ export function $<P extends Array<any>, R>(func: (...p: P) => R, onEffect?: $Eff
 export type $EffectHandler = (effect: any) => any;
 export type $EffectHandlerCreator = (parent: $EffectHandler | null) => any;
 
-export type $Scope = {
-    [$StackKey]: Array<$Variable<any>>
-}
-
-export function is$Scope(obj: any): obj is $Scope {
-    return obj.hasOwnProperty($StackKey);
-}
-
-export const $StackKey = Symbol('scope');
+export type $Scope = Array<$Variable<any>>
 
 export function $effect(effect: any) {
     if (currentScope === null) {
         throw ('No available scope.');
     }
-    const scope = currentScope;
-    currentScope = null
-    const result = scope.handler?.(effect);
-    currentScope = scope;
-    return result;
+
+    const context = getContext()
+    const handler = context[2];
+    setContext([null, null, null])
+    handler?.(effect)
+    setContext(context);
+
+    return null;
 }
 
 export function $variable<T>(init: () => T): $Variable<T> {
@@ -46,19 +38,20 @@ export function $variable<T>(init: () => T): $Variable<T> {
         throw ('No available scope.');
     }
 
-    if (currentScope[$StackKey].length < currentScope.cursor) {
-        throw (`Stack length too short. Expecting ${currentScope.cursor}, got ${currentScope[$StackKey].length}`);
+    if (currentScope.length < currentCursor!) {
+        throw (`Stack length too short. Expecting ${currentHandler}, got ${currentScope.length}`);
     }
 
-    if (currentScope[$StackKey].length === currentScope.cursor) {
-        const _scope = currentScope;
-        currentScope = null;
-        _scope[$StackKey].push(new $Variable(init()));
-        currentScope = _scope;
+    if (currentScope.length === currentCursor!) {
+        const context = getContext()
+        setContext([null, null, null])
+        const initialValue = init()
+        setContext(context)
+        currentScope.push(new $Variable(initialValue));
     }
 
-    const result = currentScope[$StackKey][currentScope.cursor]!;
-    currentScope.cursor++;
+    const result = currentScope[currentCursor!]!;
+    currentCursor = currentCursor! + 1;
     return result;
 }
 
@@ -71,33 +64,23 @@ export function $branch<T>(branch: T): $Branch<T> {
     if (currentScope === null) {
         throw ('No available scope.');
     }
-    const branches = $variable(() => new Map<T, InternalScope>()).current;
+
+    const context = getContext()
+    const branches = $variable(() => new Map<T, $Scope>()).current;
     if (!branches.has(branch)) {
-        branches.set(branch, {
-            [$StackKey]: [],
-            parent: currentScope,
-            cursor: 0,
-            handler: currentScope.handler
-        });
+        branches.set(branch, new Array<$Variable<any>>());
     }
     const newScope = branches.get(branch)!;
-    currentScope = newScope
-    currentScope.cursor = 0;
+    setContext([newScope, 0, currentHandler])
 
     return {
         branch: branch,
         branches: branches,
         get exit(): null {
-            if (currentScope === null) {
-                throw ('No available scope.');
-            }
-            if (currentScope.parent === null) {
-                throw ("Not in scope.");
-            }
             if (currentScope !== newScope) {
                 throw ('Exiting from unexpected scope.');
             }
-            currentScope = currentScope.parent;
+            setContext(context)
             return null;
         }
     } as $Branch<T>
@@ -109,10 +92,16 @@ export type $Branch<T> = {
     get exit(): null,
 }
 
-let currentScope: InternalScope | null
+let currentScope: $Scope | null
+let currentCursor: number | null
+let currentHandler: $EffectHandler | null
 
-type InternalScope = {
-    parent: InternalScope | null,
-    cursor: number,
-    handler: $EffectHandler | null
-} & $Scope
+type Context = [typeof currentScope, typeof currentCursor, typeof currentHandler];
+
+function getContext(): Context {
+    return [currentScope, currentCursor, currentHandler]
+}
+
+function setContext(context: Context) {
+    [currentScope, currentCursor, currentHandler] = [...context]
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export {$, $effect, $variable, $branch, $Variable, $StackKey, is$Scope} from './core';
+export {$, $effect, $variable, $branch, $Variable} from './core';
 export {$readPrev, $useEffect, $useMemo, $useState} from './extensions';
 
 export type {$Scope, $EffectHandler, $EffectHandlerCreator} from './core'
